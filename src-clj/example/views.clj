9671191ea893adb153c702268c0f0dfb0ed0deac
jmccaf <mccaffrey.jonathan@gmail.com>
2013-06-02 20:30:13 -0700

    progress adding more charts

--- src-clj/example/views.clj
+++ src-clj/example/views.clj
@@ -8,11 +8,54 @@
   (html5
     [:head
      [:title "Hello World!"]
-     (include-js "/js/d3.min.js")
-     (include-js "/js/crossfilter.min.js")
-     (include-js "/js/dc.min.js")
+     ; (include-js "/js/d3.min.js")
+     ; (include-js "/js/crossfilter.min.js")
+     ; (include-js "/js/dc.min.js")
+     (include-js "/js/d3.js")
+     (include-js "/js/crossfilter.js")
+     (include-js "/js/dc.js")
      (include-js "/js/jquery-1.7.1.js")
      (include-js "/js/async.js")
      (include-js "/js/main.js")]
     [:body
-     [:h1 "caliente Hello World"]]))
+     [:h1 "caliente Hello World"]
+     [:div {:id "age-histogram"}
+      [:span "Age"
+       ;; TODO look at all that filter business
+; <!-- A div anchor that can be identified by id -->
+; <div id="your-chart">
+;     <!-- Title or anything you want to add above the chart -->
+;     <span>Days by Gain or Loss</span>
+;     <!--
+;         if a link with css class "reset" is present then the chart
+;         will automatically turn it on/off based on whether there is filter
+;         set on this chart (slice selection for pie chart and brush
+;         selection for bar chart)
+;      -->
+;     <a class="reset" href="javascript:gainOrLossChart.filterAll();dc.redrawAll();" style="display: none;">reset</a>
+;     <!--
+;         dc.js will also automatically inject applied current filter value into
+;         any html element with css class set to "filter"
+;     -->
+;     <span class="reset" style="display: none;">Current filter: <span class="filter"></span></span>
+; </div>
+
+            ]]
+
+      [:div {:id "income-histogram"}
+       [:span "Household Income"]]
+      [:div {:id "bmi-histogram"}
+       [:span "Body Mass Index (BMI)"]]
+
+     ; Demographic Pies
+     [:div {:display "inline-block"}
+      [:div {:id "race-pie"}
+       [:span "Race"]]
+      [:div {:id "employment-pie"}
+       [:span "Employment Status"]]
+      [:div {:id "education-pie"}
+       [:span "Educational Attainment"]]
+      ]
+
+     ]))
+
--- src-cljs/example/hello.cljs
+++ src-cljs/example/hello.cljs
@@ -48,6 +48,13 @@
   [& ss]
   (doseq [s ss] (.log js/console s)))
 
+(defn console-log-clj
+  [& ss]
+  (doseq [s ss] 
+    (->> s
+      (clj->js)
+      (.log js/console))))
+
 (defn split-file-name-to-kw-buffer
   [file-name]
   (let [[_ s-kw s-type] (.exec #"([a-zA-Z0-9_]*)-([a-zA-Z]*)" file-name)]
@@ -80,18 +87,141 @@
     (for [idx (-> map-o-arrs
                 first 
                 (nth 1)
-                (.-length)
+                (.-length) 
                 range)]
       (into {}
             (for [[kw arr] map-o-arrs]
               [kw (aget arr idx)])))))
 
+(def brfss-llcp-chart-group "brfss-llcp-group")
+
+(defn count-to-percentage-fn
+  [group-all]
+  (fn [d]
+    (-> d
+      (.-value)
+      (* 100)
+      (/ (.value group-all)))))
+
+(defn percentage-scale
+  []
+  (-> js/d3
+    (.-scale)
+    (.linear)
+    (.domain
+      (clj->js [0 100]))))
+
+(def histogram-margins
+  (clj->js {:top 10 :right 10 :bottom 30 :left 50}))
+
+(defn print-dimension-min-max
+  [tag dim]
+  (console-log-clj
+    {:tag tag
+     :min (aget (aget (.bottom dim 1) 0) tag)
+     :max (aget (aget (.top dim 1) 0) tag)}))
+
+(defn make-age-histogram
+  [anchor-tag chart-group age-dim age-group-count age-group-all-count]
+  (print-dimension-min-max "AGE" age-dim)
+  (doto (.barChart js/dc anchor-tag chart-group)
+    (.dimension age-dim)
+    (.group age-group-count)
+    (.valueAccessor (count-to-percentage-fn age-group-all-count))
+    (.width 1000)
+    (.height 300)
+    (.margins histogram-margins)
+    (.x (-> js/d3 (.-scale) (.linear) (.domain
+                                        (clj->js [-1 100]))))
+    (.y (percentage-scale))
+    (.centerBar true)
+    (.title #(str (.-value %) " years old"))))
+
+(defn make-income-histogram
+  [anchor-tag chart-group income-dim income-group-count income-group-all-count]
+  (print-dimension-min-max "INCOME2" income-dim)
+  (doto (.barChart js/dc anchor-tag chart-group)
+    (.dimension income-dim)
+    (.group income-group-count)
+    (.valueAccessor (count-to-percentage-fn income-group-all-count))
+    (.width 1000)
+    (.height 300)
+    (.round (fn [x]
+              (if (< x 0)
+                x
+                (-> x (/ 10) int (* 10)))))
+    (.margins histogram-margins)
+    (.x (-> js/d3 (.-scale) (.linear) (.domain
+                                        (clj->js [-1 100]))))
+    (.y (percentage-scale))
+    ; TODO finish
+    (.centerBar true)
+  ))
+
+(defn make-basic-pie
+  [anchor-tag chart-group dim group]
+  (doto (.pieChart js/dc anchor-tag chart-group)
+    (.width 200)
+    (.height 200)
+    (.radius 90)
+    (.innerRadius 40)
+    (.dimension dim)
+    (.group group)
+    ; TODO pretty-print
+    (.label #(-> %1 (.-data) (.-key)))
+    (.renderLabel true)
+    ; TODO pretty-print
+    (.title #(-> %1 (.-data) (.-key)))
+    (.renderTitle true)))
+
+(def make-race-pie make-basic-pie)
+(def make-employment-pie make-basic-pie)
+(def make-education-pie make-basic-pie)
+
+(defn dim-count
+  [ndx dim-fn]
+  (let [dim (.dimension ndx dim-fn)]
+    [dim
+     (-> dim
+       (.group)
+       (.reduceCount))]))
+
+; TODO!  Group-all should not ignore all filters!
+(defn dim-count-all-count
+  [ndx dim-fn]
+  (let [[dim & _ :as inner-vec] (dim-count ndx dim-fn)]
+    (conj inner-vec (-> dim
+                      (.groupAll)
+                      (.reduceCount)))))
+
+; TODO factory function for dim/group/group-all creation
 (defn setup-crossfilter
   [data]
   (let [ndx (js/crossfilter data)
-        age-dim (.dimension ndx (fn [d] (aget d "AGE")))]
-    (console-log (.top age-dim 10))
-    (console-log (.bottom age-dim 10))))
+        [age-dim age-group-count age-group-all-count]
+        (dim-count-all-count ndx (fn [d] (aget d "AGE")))
+        [income-dim income-group-count income-group-all-count]
+        (dim-count-all-count ndx (fn [d] (aget d "INCOME2")))
+        [race-dim race-group-count]
+        (dim-count ndx (fn [d] (aget d "RACE")))
+      ;  [education-dim education-group-count]
+      ;  (dim-count ndx (fn [d] (aget d "EDUCA")))
+        [employment-dim employment-group-count]
+        (dim-count ndx (fn [d] (aget d "EMPLOY")))
+        ]
+    (make-age-histogram "#age-histogram" brfss-llcp-chart-group
+      age-dim age-group-count age-group-all-count)
+    (make-income-histogram "#income-histogram" brfss-llcp-chart-group
+      income-dim income-group-count income-group-all-count)
+    (make-race-pie "#race-pie" brfss-llcp-chart-group
+      race-dim race-group-count)
+;    (make-education-pie "#education-pie" brfss-llcp-chart-group
+;      education-dim education-group-count)
+    (make-employment-pie "#employment-pie" brfss-llcp-chart-group
+      employment-dim employment-group-count)
+    (.renderAll js/dc brfss-llcp-chart-group)
+    (console-log "Done with crossfilter.")
+    ))
 
 (.map js/async
       (clj->js file-list)
